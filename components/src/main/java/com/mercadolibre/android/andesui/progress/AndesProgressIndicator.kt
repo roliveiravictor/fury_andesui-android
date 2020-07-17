352b3b4fa2b1b92488317f138ff22c5bb71e42d7
Victor R <victor.rocha@mercadolivre.com>
2020-07-17 18:16:05 -0300

    added prog initAttrs for AndesProgress

--- components/src/main/java/com/mercadolibre/android/andesui/progress/AndesProgressIndicator.kt
+++ components/src/main/java/com/mercadolibre/android/andesui/progress/AndesProgressIndicator.kt
@@ -1,9 +1,16 @@
 package com.mercadolibre.android.andesui.progress
 
 import android.content.Context
+import android.support.annotation.DrawableRes
 import android.support.constraint.ConstraintLayout
 import android.util.AttributeSet
 import com.mercadolibre.android.andesui.R
+import com.mercadolibre.android.andesui.checkbox.AndesCheckbox
+import com.mercadolibre.android.andesui.checkbox.align.AndesCheckboxAlign
+import com.mercadolibre.android.andesui.checkbox.factory.AndesCheckboxAttrs
+import com.mercadolibre.android.andesui.checkbox.factory.AndesCheckboxConfigurationFactory
+import com.mercadolibre.android.andesui.checkbox.status.AndesCheckboxStatus
+import com.mercadolibre.android.andesui.checkbox.type.AndesCheckboxType
 import com.mercadolibre.android.andesui.color.AndesColor
 import com.mercadolibre.android.andesui.progress.extensions.get
 import com.mercadolibre.android.andesui.progress.factory.AndesProgressIndicatorAttrs
@@ -21,9 +28,24 @@ class AndesProgressIndicator : ConstraintLayout {
         initAttrs(attrs)
     }
 
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : this(context, attrs)
+    constructor(
+            context: Context,
+            size: AndesProgressIndicatorSize,
+            tint: AndesColor,
+            textColor: AndesColor,
+            label: String,
+            linear: Boolean,
+            offset: Int = 0,
+            @DrawableRes thumbnail: Int
+    ) : super(context) {
+        initAttrs(size, tint, textColor, label, linear, offset, thumbnail)
+    }
 
-    fun create() {
+
+    /*
+    * Test Method - Will be removed
+    * */
+    private fun create() {
         val progress = AndesProgressIndicatorConfigurationFactory.create(context,
                 AndesProgressIndicatorAttrs(
                         AndesProgressIndicatorSize.SMALL,
@@ -39,12 +61,41 @@ class AndesProgressIndicator : ConstraintLayout {
 
     private fun initAttrs(attrs: AttributeSet?) {
         andesProgressIndicatorAttrs = AndesProgressIndicatorAttrsParser.parse(context, attrs)
-        setupComponents(createConfig())
+        setupComponents(
+                AndesProgressIndicatorConfigurationFactory.create(
+                        context,
+                        andesProgressIndicatorAttrs
+                )
+        )
+    }
+
+    private fun initAttrs(
+            size: AndesProgressIndicatorSize,
+            tint: AndesColor,
+            textColor: AndesColor,
+            label: String,
+            linear: Boolean,
+            offset: Int = 0,
+            @DrawableRes thumbnail: Int
+    ) {
+        andesProgressIndicatorAttrs = AndesProgressIndicatorAttrs(
+                size,
+                tint,
+                textColor,
+                label,
+                linear,
+                offset,
+                thumbnail
+        )
+        setupComponents(
+                AndesProgressIndicatorConfigurationFactory.create(
+                        context,
+                        andesProgressIndicatorAttrs
+                )
+        )
     }
 
     private fun setupComponents(config: AndesProgressIndicatorConfiguration) {
 
     }
-
-    private fun createConfig() = AndesProgressIndicatorConfigurationFactory.create(context, andesProgressIndicatorAttrs)
 }
